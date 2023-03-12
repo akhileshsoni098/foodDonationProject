73e7b61257652cc773a4aeddb686c7ffa1bbb0e8
Akhilesh Soni <117471855+akhileshsoni098@users.noreply.github.com>
2023-03-13 00:40:47 +0530

    Update FoodDonor.js

--- src/controller/FoodDonor.js
+++ src/controller/FoodDonor.js
@@ -1,50 +1,8 @@
 const donorModel = require("../models/donerModel")
 
 
-/* 
-donorName:{
-    type:String,
-    required:true
-},
-foodType:{
-    type:String,
-    required:true
-},
-quantity:{
-    type:Number,
-    required:true
-}, 
-pickUpAddress:{   // from wherre to pick 
-    type:String,
-    required: true
-},
-pickUpDate:{   //  picking date 
-    type:Date,
-    required:true
-},
-recipientName:{   // receiver Name
-    type:String,
-    required:true
-},
-recipientAddress:{  // reciver address 
-    type:String,
-    required:true
-},
-recipientPhone:{
-    type:String,
-    required: true
-},
-status: {
-    type: String,
-    enum: ['pending', 'picked up', 'delivered'],
-    default: 'pending'
-  },
-  notes: {
-    type: String
-  }
-});
 
-*/
+
 const FoodDonor = async (req, res)=>{
 try{
 const data = req.body
